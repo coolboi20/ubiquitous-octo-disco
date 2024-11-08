1e793f9c604bb72546f7b16a1823909a05c0d7d6
mmahase <mmahase@oldmutual.com>
2024-11-08 12:18:18 +0000

    Adding knobs

--- widgetbook/lib/navigation_icon_use_cases.dart
+++ widgetbook/lib/navigation_icon_use_cases.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 import 'package:boost/components/navigation_icon.dart';
+import 'package:widgetbook_workspace/knobs/icon_knob.dart';
 
 @widgetbook.UseCase(
   designLink: 'https://www.figma.com/file/LuHhOTyQBglXOYK5b5VRSM/Widget-book-test?node-id=2019-602',
@@ -9,10 +11,21 @@ import 'package:boost/components/navigation_icon.dart';
   type: NavigationIcon
 )
 Widget defaultNavigationIconUseCase(BuildContext context) {
-  return const NavigationIcon(
-    icon: Icons.home,
-    color: Colors.grey,
-    size: 24.0,
+  return NavigationIcon(
+    icon: context.knobs.icon(
+      label: 'Icon',
+      initialValue: Icons.home,
+    ),
+    color: context.knobs.color(
+        label: 'Icon Color',
+      initialValue: Colors.grey,
+    ),
+    size: context.knobs.double.slider(
+      label:'Icon Size',
+      initialValue: 24.0,
+      min: 16.0,
+      max: 48.0,
+    ),
   );
 }
 
@@ -22,9 +35,20 @@ Widget defaultNavigationIconUseCase(BuildContext context) {
     type: NavigationIcon
 )
 Widget selectedNavigationIconUseCase(BuildContext context) {
-  return const NavigationIcon(
-    icon: Icons.home,
-    color: Colors.black,
-    size: 24.0,
+  return NavigationIcon(
+    icon: context.knobs.icon(
+      label: 'Icon',
+      initialValue: Icons.home,
+    ),
+    color: context.knobs.color(
+      label: 'Icon Color',
+      initialValue: Colors.black,
+    ),
+    size: context.knobs.double.slider(
+      label:'Icon Size',
+      initialValue: 24.0,
+      min: 16.0,
+      max: 48.0,
+    ),
   );
 }
